c6c597c45bfbd3e592c662088bb49ed01ff1ba4b
vitorwho@outlook.com <vitorwho@outlook.com>
2025-03-30 13:54:07 -0300

    todo esta done

--- main/java/br/com/locadora/LocadoraApplication.java
+++ main/java/br/com/locadora/LocadoraApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LocadoraApplication {
-	// TODO: Adicionar título na classe de Dados do filme
 	public static void main(String[] args) {
 		SpringApplication.run(LocadoraApplication.class, args);
 	}
